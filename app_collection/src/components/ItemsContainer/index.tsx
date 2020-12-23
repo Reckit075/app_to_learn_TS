4a291064cc6ad6f60a0122c526766ce539178f3b
Reckit075 <mateusz.majka.zsl@gmail.com>
2020-12-23 19:23:44 +0100

    create container for elements

---
+++ app_collection/src/components/ItemsContainer/index.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react'
+import './index.css'
+
+const ItemsContainer:FC = () =>{
+    return(
+        <div className='ItemsContainer'></div>
+    );
+}
+
+export default ItemsContainer
